3a7a1cbad33a293db2cf17c929027bf71d25dd30
caroapaza <apazacarolina@gmail.com>
2024-04-22 19:16:25 -0300

    Se completa el ejercicio1

--- tp2_ApazaCarolina/src/ar/edu/unju/fi/main/Main.java
+++ tp2_ApazaCarolina/src/ar/edu/unju/fi/main/Main.java
@@ -1,9 +1,22 @@
 package ar.edu.unju.fi.main;
 
+import ar.edu.unju.fi.model.Semaforo;
+import ar.edu.unju.fi.model.Semaforo.ColorSemaforo;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// uso enum
+		
+		Semaforo semaforo1 = new Semaforo();
+		System.out.println(semaforo1);
+		System.out.println("El color del semaforo es: "+semaforo1.getColor().toString());
+		
+		Semaforo semaforo2 = new Semaforo(ColorSemaforo.AMARILLO,true);
+		
+		semaforo1.setColor(ColorSemaforo.VERDE);
+		System.out.println(semaforo1);
+	
 
 	}
 
